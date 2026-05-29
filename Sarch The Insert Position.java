17248459bc2e7f5e5919c61e8520f504182857fc
Rahul Singh <rahul8287988137@gmail.com>
2026-05-29 18:18:05 +0530

    Add files via upload

---
+++ Sarch The Insert Position.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int searchInsert(int[] nums, int target) {
+        int lb = 0 , ub = nums.length - 1;
+        while(lb<=ub){
+            int mid = lb + (ub - lb)/2;
+            if(nums[mid] == target){
+                return mid;
+            }
+            else if(nums[mid]<target){
+                lb = mid + 1;
+            }
+            else{
+
+                ub = mid - 1;
+            }
+        
+        }
+        return lb;
+    }
+}
