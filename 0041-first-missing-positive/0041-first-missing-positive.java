0489905b98edb365b7c881901772ccea652882f2
Rahul Singh <rahul8287988137@gmail.com>
2026-07-22 14:56:14 +0530

    Time: 1 ms (99.98%), Space: 71.1 MB (85.85%) - LeetHub

---
+++ 0041-first-missing-positive/0041-first-missing-positive.java
@@ -0,0 +1,25 @@
+class Solution {
+void swap(int[] arr,int i, int j){
+    int temp = arr[i];
+    arr[i] = arr[j];
+    arr[j] = temp;
+}
+    public int firstMissingPositive(int[] arr) {
+        int i=0;
+        int n = arr.length;
+        while(i<n){
+            if(arr[i]<=0 || arr[i]>n || arr[i] == i+1 || arr[i] == arr[arr[i]-1]){
+                i++;
+            }
+            else{
+                swap(arr,i,arr[i]-1);
+            }
+        }
+        for(i =0; i<n ; i++){
+            if(arr[i]!= i+1){
+                return i+1;
+            }
+        }
+        return n+1;
+    }
+}
